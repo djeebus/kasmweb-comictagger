cd0f091560f2595984f2690794c228dca3417cc7
Joe Lombrozo <joe@djeebus.net>
2023-01-07 17:05:26 -0500

    install the 1.5.5 wheel directly

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 set -e
 set -x
 
-pip3 install 'comictagger[all]==1.5.5'
+wget https://github.com/comictagger/comictagger/releases/download/1.5.5/comictagger-1.5.5-py3-none-any.whl
+pip3 install ./comictagger-1.5.5-py3-none-any.whl
 
 rm -rf /var/lib/apt/lists/*
